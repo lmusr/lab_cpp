0d92eddd2bc2e274f49952e4d3f55e7e3d1316fd
lmusr <54545418+lmusr@users.noreply.github.com>
2023-09-08 11:03:58 +0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>  //i/o library
+using namespace std; //instead od prefix std::
+
+int main() //header
+{ //begin
+	float a, res; //declaration
+	//input
+	cout << "a = ";
+	cin >> a;
+	//calculation
+	res = a + 5;
+	//output
+	cout << "a+5 = " << res << endl;
+	system("pause");
+	return 0; //return to OS
+} //end
